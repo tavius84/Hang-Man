75782356abfae0ff985e00365f247570125129da
tavius84 <goldbrass8@gmail.com>
2020-03-13 13:06:39 -0500

    Initial commit

---
+++ src/HangMan.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class HangMan {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        String userInput;
+        //HELLO
+
+        
+
+    }
+}
