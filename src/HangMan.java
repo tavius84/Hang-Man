06db6e4535bc5e908d98f72d01b380c6819d6f0c
tavius84 <goldbrass8@gmail.com>
2020-03-13 13:55:20 -0500

    1

--- src/HangMan.java
+++ src/HangMan.java
@@ -2,11 +2,22 @@ import java.util.Scanner;
 
 public class HangMan {
     public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        String userInput;
-        //HELLO
+        Scanner userInput = new Scanner(System.in);
+        int r = ((int)Math.random()*3);
+        String[] wordBank = {"Cuba, China, Laos"};
+        String word = wordBank[r];
+        int wordLength;
+        int attempts = 10;
+        boolean solved;
 
-        
+
+
+
+
+
 
     }
+
+
+
 }
---
+++ src/HangmanComponent.java
@@ -0,0 +1,5 @@
+public class HangmanComponent {
+    public static void main(String[] args) {
+        
+    }
+}
